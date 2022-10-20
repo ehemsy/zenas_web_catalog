3ea6cd00a42eb2f119ee3ec3920894699982b5d4
ehemsy <116014216+ehemsy@users.noreply.github.com>
2022-10-20 15:45:31 -0500

    Update zenas_web_catalog.py
    Add text

--- zenas_web_catalog.py
+++ zenas_web_catalog.py
@@ -1,4 +1,9 @@
+# Import dependencies
 import streamlit
+import snowflake.connector
+
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog!')
+streamlit.text('Pick a sweatsuit color or style:')
+
 
